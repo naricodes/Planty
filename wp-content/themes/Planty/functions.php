b87efe335a7ef539e7618cf2d8e99e31be860ce7
Narimene <zianenarimane1@gmail.com>
2023-02-13 11:04:39 +0100

    added more features

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -22,3 +22,27 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+
+// link for admin
+
+function wp_admin_link_for_admins( $items, $args ) {
+    if ( is_admin() || current_user_can( 'manage_options' ) ) {
+    $items .= '<li id="menu-item-185" class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="'. admin_url() .'" class="show-title hfe-menu-item"> Admin </a></li>';
+    }
+    return $items;
+    }
+    // 10 is a priority rule
+    // 2 is the number of argument(s)
+    add_filter( 'wp_nav_menu_items', 'wp_admin_link_for_admins', 10, 2 );
+    
+    // adding css
+    function nm_stylesheet() {
+        wp_enqueue_style( 'my-stylesheet', get_template_directory_uri() . 'style.css' );
+        }
+        add_action( 'wp_enqueue_scripts', 'nm_stylesheet' );
+
+
+
+        // concatenation
+        // + . 
